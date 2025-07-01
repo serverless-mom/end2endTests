4f39f326eaf8a66c9ac0f4762cdce942866c8610
Nocnica Mellifera <noctnica@gmail.com>
2025-07-01 15:20:47 -0700

    less strict testing

--- __checks__/canEditDocs.spec.ts
+++ __checks__/canEditDocs.spec.ts
@@ -4,6 +4,6 @@ const { expect, test } = require('@playwright/test')
 test('nav to docs, then click edit link in docs', async ({ page }) => {
   await page.goto('https://www.checklyhq.com/');
   await page.getByRole('button', { name: 'Resources' }).first().click();
-  await page.getByRole('link', { name: 'Documentation', exact: true }).click();
-  await expect(page.getByRole('article')).toContainText('this page on Github');
+  await page.getByRole('link', { name: 'Documentation'}).first().click();
+  await expect(page.getByRole('article')).toContainText('on Github');
 })
