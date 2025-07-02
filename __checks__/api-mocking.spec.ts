d363741bae8769f5d5349b824b9e9cc5055fb05c
Nocnica Mellifera <noctnica@gmail.com>
2025-07-02 07:43:37 -0700

    update tests to pass with shop updates

--- __checks__/api-mocking.spec.ts
+++ __checks__/api-mocking.spec.ts
@@ -4,7 +4,7 @@ test('Book has correct details', async ({ page }) => {
   await page.route('*/**/api/books/23', async route => {
     const response = await route.fetch();
     const json = await response.json();
-    json.stock="10980982349082340892340"
+    json.stock="0"
     await route.fulfill({ response, json });
   });
   await page.goto('https://danube-webshop.herokuapp.com/books/23');
